fa4db37e79ef61b34534c544c146fccc2eb12e85
Julian Feliciano <julsfelic@gmail.com>
2016-02-04 22:08:42 -0700

    Finish college scorecard

--- lib/college_scorecard.rb
+++ lib/college_scorecard.rb
@@ -6,23 +6,31 @@ class CollegeScorecard
     CSV.open('data/2013_college_scorecards.csv', headers: true, header_converters: :symbol)
   end
 
-  def by_state(csv)
+  def by_state(args, csv)
     state_colleges = csv.select do |row|
-      ARGV[1] == row[:stabbr]
+      args[1] == row[:stabbr]
     end
     state_colleges.each { |row| puts row[:instnm] }
   end
 
-  def top_average_faculty_salary(csv)
+  def top_average_faculty_salary(args, csv)
     state_colleges = csv.sort_by do |row|
-      row[:avgfacsal]
-    end[0..9]
+      row[:avgfacsal].to_i
+    end.reverse[0...(args[1].to_i)]
     state_colleges.each { |row| puts row[:instnm] }
+  end
+
+  def median_debt_between(args, csv)
+    state_colleges = csv.select do |row|
+      row[:grad_debt_mdn].to_i.between?(args[1].to_i, args[2].to_i)
+    end
+    sorted = state_colleges.sort_by { |row| row[:grad_debt_mdn] }.reverse
+    sorted.each { |row| puts "#{row[:instnm]} ($#{row[:grad_debt_mdn]})" }
   end
 end
 
 if __FILE__ == $0
   college_scorecard = CollegeScorecard.new
   csv = college_scorecard.open_file
-  college_scorecard.send(ARGV[0], csv)
+  college_scorecard.send(ARGV[0], ARGV, csv)
 end
