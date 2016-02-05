e55bdf5507d4232a3163550244c92bb953d8020f
Julian Feliciano <julsfelic@gmail.com>
2016-02-05 08:19:26 -0700

    Final version

--- test/college_scorecard_test.rb
+++
@@ -1,9 +0,0 @@
-require "minitest/autorun"
-require_relative "../lib/college_scorecard"
-require 'pry'
-
-class CollegeScorecardTest < Minitest::Test
-  def test_college_scorecard_works
-    var = CollegeScorecard.new
-  end
-end
